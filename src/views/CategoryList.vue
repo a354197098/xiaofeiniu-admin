6d984ea506305f707930528085c83180432ab7a7
lichanghong <354197098@qq.com>
2019-02-11 20:10:42 +0800

    管理后台

--- src/views/CategoryList.vue
+++ src/views/CategoryList.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="">
-    <h1>CategoryList</h1>
+  <div class="xfn-category-list">
+    <el-breadcrumb>
+      <el-breadcrumb-item to="/main">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>菜品类别管理</el-breadcrumb-item>
+      <el-breadcrumb-item>菜品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <br/>
+    <el-button type="primary">添加新的菜品类别</el-button>
+    <br/>
+    <el-table :data="categoryList" stripe border>
+      <el-table-column label="编号" prop="cid"></el-table-column>
+      <el-table-column label="名称" prop="cname"></el-table-column>
+      <el-table-column label="操作">
+
+        <template slot-scope="c">
+        <el-button size="mini" @click="deleteCategory(c.cid)" type="success">修改</el-button>
+        <el-button size="mini" @click="updateCategory(c.cid)" type="error">删除</el-button>
+        </template>
+
+      </el-table-column>
+    </el-table>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      categoryList:[]
+    }
+  },
+  methods:{
+    deleteCategory(cid){
+      console.log(cid)
+    },
+    updateCategory(cid){
+
+    }
+  },
+  mounted(){
+    var url = this.$store.state.globalSettings.apiUrl + '/admin/category'
+    this.$axios.get(url).then((res)=>{
+      this.categoryList = res.data
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+}
+</script>
